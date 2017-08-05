115e1157c7d2a5023f6f44308aee77a452d968c1
mkapiczy <mix.kap@gmail.com>
2017-08-05 14:05:40 +0200

    Deleting FacebookPojo and FacebookRepository files

--- src/main/java/com/github/britter/springbootherokudemo/repository/FacebookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.britter.springbootherokudemo.repository;
-
-import com.github.britter.springbootherokudemo.entity.FaceBookPojo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacebookRepository extends JpaRepository<FaceBookPojo, Long> {
-}
