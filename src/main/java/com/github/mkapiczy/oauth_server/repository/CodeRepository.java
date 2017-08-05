d630bc6838c1d1b3012967b659bde257ecfdf903
Michał Kapiczyński <mix.kap@gmail.com>
2017-08-05 22:16:33 +0200

    CodeRepository - findByCode method

--- src/main/java/com/github/mkapiczy/oauth_server/repository/CodeRepository.java
+++ src/main/java/com/github/mkapiczy/oauth_server/repository/CodeRepository.java
@@ -4,8 +4,12 @@ import com.github.mkapiczy.oauth_server.entity.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CodeRepository extends JpaRepository<Code, Long> {
+
+    List<Code> findByCode(String code);
 
 
 }
