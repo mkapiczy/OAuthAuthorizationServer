d6e567a998c075125ac1b80aea9b025f6db15949
mkapiczy <mix.kap@gmail.com>
2017-08-05 14:44:14 +0200

    Service for generating random String codes

---
+++ src/main/java/com/github/britter/springbootherokudemo/service/RandomCodeGeneratorService.java
@@ -0,0 +1,17 @@
+package com.github.britter.springbootherokudemo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+@Service
+public class RandomCodeGeneratorService {
+
+    private SecureRandom random = new SecureRandom();
+
+    public String generateRandom32SignCode(){
+        return new BigInteger(130, random).toString(32);
+    }
+
+}
