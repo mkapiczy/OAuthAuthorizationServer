11cdf792a7de5996dd9538e669d082947d9c5587
mkapiczy <mix.kap@gmail.com>
2017-08-05 14:44:45 +0200

    Repository method for finding register app by appId

--- src/main/java/com/github/britter/springbootherokudemo/repository/RegisteredAppRepository.java
+++ src/main/java/com/github/britter/springbootherokudemo/repository/RegisteredAppRepository.java
@@ -2,8 +2,13 @@ package com.github.britter.springbootherokudemo.repository;
 
 import com.github.britter.springbootherokudemo.entity.RegisteredApp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RegisteredAppRepository extends JpaRepository<RegisteredApp, Long> {
+
+    List<RegisteredApp> findByAppId(String appId);
 }
